39cbbb7b35f59eaf2744d11b57b0f8dedeb30a67
unknown <mchua004@gmail.com>
2021-06-05 02:52:48 -0400

    Finalized logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,16 +1,16 @@
 // Create a map object
 var map = L.map("mapid", {
-    center: [40, -100],
-    zoom: 5
-  });
+  center: [39.8283, -98.5795],
+  zoom: 5
+});
 
-  // the base map
+// adding the base map
 L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    maxZoom: 10,
-    id: "light-v10",
-    accessToken: API_KEY
-  }).addTo(map);
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  maxZoom: 18,
+  id: "light-v10",
+  accessToken: API_KEY
+}).addTo(map);
 
 function Markers(response){
 	console.log(response)
@@ -22,18 +22,71 @@ function Markers(response){
 			
 			//inset data in the popup
 			var date = new Date(response.features[i].properties.time*1000)
-			var pl = response.features[i].properties.place
+			var loc = response.features[i].properties.place
 			var mag = response.features[i].properties.mag
 
         // Create the circles for each earthquake report and add to the baseMap layer.
-        L.dots(coords, {
-            opacity: .7,
-            fillOpacity: 0.80,
-            weight: .7,
-            color: 'red',
+        L.circle(coords, {
+            opacity: .5,
+            fillOpacity: 0.75,
+            weight: .5,
+            color: 'black',
             fillColor: colorDepth(depth),
             radius: 7000 * response.features[i].properties.mag
     }).bindPopup(`<p align = "left"> <strong>Date:</strong> ${date} <br>
-     <strong>Location:</strong> ${pl} <br> <strong>Magnitude:</strong> ${mag} </p>`).addTo(map)
+     <strong>Location:</strong> ${loc} <br> <strong>Magnitude:</strong> ${mag} </p>`).addTo(map)
 	};
 };
+//function to picke the color
+function colorDepth(depth){
+	switch(true){
+            case (depth > -10 && depth < 10):
+                color = 'rgb(24, 235, 45)'
+                break;
+            case (depth >= 10 && depth < 30):
+                color = 'rgb(143, 206, 0)'
+                break;
+            case (depth >= 30 && depth < 50):
+                color = 'rgb(191, 174, 0)'
+                break;
+            case (depth >= 50 && depth < 70):
+                color = 'rgb(223, 136, 0)'
+                break;
+            case ( depth >= 70 && depth < 90):
+                color = 'rgb(242, 91, 0)'
+                break;
+            case (depth >= 90):
+                color = 'rgb(247, 24, 31)'
+                break;
+        };
+		return color
+};
+newMarker = L.layer
+//function to create the legend
+function Legend(){
+    var div = L.DomUtil.create('div', 'info legend');
+    var grades = ['-10-10', '10-30', '30-50', '50-70', '70-90', '90+'];
+    var colors = [
+        'rgb(24, 235, 45)',
+        'rgb(143, 206, 0)',
+        'rgb(191, 174, 0)',
+        'rgb(223, 136, 0)',
+        'rgb(242, 91, 0)',
+        'rgb(247, 24, 31)'
+        ];
+    var labels = [];
+    // loop through our density intervals and generate a label with a colored square for each interval
+    grades.forEach(function(grade, index){
+        labels.push("<div class = 'row'><li style=\"background-color: " + colors[index] +  "; width: 10px"+ "; height: 10px" + "\"></li>" + "<li>" + grade + "</li></div>");
+    })
+  
+    div.innerHTML += "<ul>" + labels.join("") +"</ul>";
+    return div;
+};
+//adding a legend to the map
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = Legend
+legend.addTo(map)
+
+//call to fetch all the data to show on the map
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson").then(Markers);
