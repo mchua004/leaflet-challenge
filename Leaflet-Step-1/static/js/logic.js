7aa61f790ebe22f66c5089476575375761bbfecf
unknown <mchua004@gmail.com>
2021-06-05 02:28:15 -0400

    Added markers function

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -11,3 +11,29 @@ L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?acce
     id: "light-v10",
     accessToken: API_KEY
   }).addTo(map);
+
+function Markers(response){
+	console.log(response)
+	for (var i=0; i< response.features.length; i++){
+			//retrieve the coordinates for each marker
+			coords = [response.features[i].geometry.coordinates[1], response.features[i].geometry.coordinates[0]]
+			// retrieving the depth of the earthquake - it will determine the color of the circle
+			var depth = response.features[i].geometry.coordinates[2];
+			
+			//inset data in the popup
+			var date = new Date(response.features[i].properties.time*1000)
+			var pl = response.features[i].properties.place
+			var mag = response.features[i].properties.mag
+
+        // Create the circles for each earthquake report and add to the baseMap layer.
+        L.dots(coords, {
+            opacity: .7,
+            fillOpacity: 0.80,
+            weight: .7,
+            color: 'red',
+            fillColor: colorDepth(depth),
+            radius: 7000 * response.features[i].properties.mag
+    }).bindPopup(`<p align = "left"> <strong>Date:</strong> ${date} <br>
+     <strong>Location:</strong> ${pl} <br> <strong>Magnitude:</strong> ${mag} </p>`).addTo(map)
+	};
+};
