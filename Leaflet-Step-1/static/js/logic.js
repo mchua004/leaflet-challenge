9d62d57259cb1f830f5397d89a21d2431c6e9921
unknown <mchua004@gmail.com>
2021-06-05 02:25:24 -0400

    added base map

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,5 +1,13 @@
 // Create a map object
 var map = L.map("mapid", {
-    center: [39.8283, -98.5795],
+    center: [40, -100],
     zoom: 5
   });
+
+  // the base map
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    maxZoom: 10,
+    id: "light-v10",
+    accessToken: API_KEY
+  }).addTo(map);
